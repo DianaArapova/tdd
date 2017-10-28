080117da31ea4a808a268a1449c036a7936ebc39
DianaArapova <diiana_arapova@mail.ru>
2017-10-28 21:47:58 +0500

    Add First Test

--- TagsCloudVisualization/CircularCloudLayouter.cs
+++ TagsCloudVisualization/CircularCloudLayouter.cs
@@ -9,24 +9,46 @@ using FluentAssertions;
 
 namespace TagsCloudVisualization
 {
-	class CircularCloudLayouter
+	public class CircularCloudLayouter
 	{
 		private static Point Center;
+		private static List<Rectangle> cloudOfRectangles;
 
-		CircularCloudLayouter(Point center)
+		public CircularCloudLayouter(Point center)
 		{
 			Center = center;
+			cloudOfRectangles = new List<Rectangle>();
 		}
 
-		Rectangle PutNextRectangle(Size rectangleSize)
+		public Rectangle PutNextRectangle(Size rectangleSize)
 		{
+			if (cloudOfRectangles.Count == 0)
+			{
+				cloudOfRectangles.Add(GetFirstRectangle(rectangleSize));
+				return GetFirstRectangle(rectangleSize);
+			}
 			return new Rectangle();
+		}
+
+		private Rectangle GetFirstRectangle(Size rectangleSize)
+		{
+			return new Rectangle(Center.X - rectangleSize.Width / 2, 
+				Center.Y - rectangleSize.Height / 2, 
+				rectangleSize.Width, 
+				rectangleSize.Height);
 		}
 	}
 
 	[TestFixture]
 	class CircularCloudLayouter_Should
 	{
-		
+		[Test]
+		public void CenterOfFirstRactangle_IsSutuated_InCenterOfCloud()
+		{
+			var cloud = new CircularCloudLayouter(new Point(12, 12));
+			var rectangle = new Rectangle(10, 10, 4, 4);
+			cloud.PutNextRectangle(new Size(4, 4)).ShouldBeEquivalentTo(rectangle);
+
+		}
 	}
 }
