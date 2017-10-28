3dd9eca4dc0fdcd488561f9b8b2ff59c1490bfe1
DianaArapova <diiana_arapova@mail.ru>
2017-10-28 21:11:06 +0500

    create Cloud

---
+++ TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace TagsCloudVisualization
+{
+	class CircularCloudLayouter
+	{
+		private static Point Center;
+
+		CircularCloudLayouter(Point center)
+		{
+			Center = center;
+		}
+
+		Rectangle PutNextRectangle(Size rectangleSize)
+		{
+			return new Rectangle();
+		}
+	}
+
+	[TestFixture]
+	class CircularCloudLayouter_Should
+	{
+		
+	}
+}
